ad2851e0aba4081a58a36f2966b3447c29f0f0fe
Alex Hermsen <a.hermze@gmail.com>
2025-02-25 21:56:01 -0600

    map works now

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from backend.func_new import *
 import os
 from lib2to3.pgen2.pgen import DFAState
@@ -5,14 +6,12 @@ from flask import Flask, request, redirect, url_for, render_template, flash, Blu
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
 
-
 app = Flask(__name__)
 Bootstrap(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 OUTPUT_DIR = "static"
 upload_dir = "./excel_files/"
 app.config["UPLOAD_FOLDER"] = "./excel_files/"
-
 
 @app.route("/")
 def index():
@@ -32,6 +31,16 @@ def upload():
 @app.route("/profile")
 def profile():
     return render_template("/base/profile.html")
+
+@app.route("/moreinfo")
+def moreinfo():
+    return render_template("/base/moreinfo.html")
+
+@app.route("/themap")
+def themap():
+    # if request.method == ["POST"]:
+
+    return render_template("/base/themap.html")
 
 
 @app.route("/upload", methods=["POST"])
@@ -78,7 +87,7 @@ def upload_success():
 def upload_success_2nd():
     if request.method == "POST":
         full_name = upload_dir + filename_
-        global treatment
+        global treatment    
         global start_DOY
         global end_DOY
         start_hour = request.form["start_hour"]
